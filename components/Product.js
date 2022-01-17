0f663dc00c37648a0172df4aad060202ba209ea0
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-17 14:12:41 +0900

    style css for product

--- components/Product.js
+++ components/Product.js
@@ -13,31 +13,35 @@ const Product = ({ id, title, price, description, category, image }) => {
   const [hasPrime, setHasPrime] = useState(Math.random() < 0.5);
 
   return (
-    <div>
-      <p>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
       <Image src={image} width={200} height={200} objectFit="contain" alt="" />
-      <h4>{title}</h4>
+      <h4 className="my-3">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
           .map((_, i) => (
-            <StarIcon className="h-5" key={i} />
+            <StarIcon className="h-5 text-yellow-500" key={i} />
           ))}
       </div>
 
-      <p>{description}</p>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-      <div>
+      <div className="mb-5">
         {new Intl.NumberFormat("ja-JP", {
           style: "currency",
           currency: "JPY",
         }).format(price)}
       </div>
       {hasPrime && (
-        <div>
-          <img src="https://links.papareact.com/fdw" alt="" />
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
+      <button className="mt-auto button">Add to Basket</button>
     </div>
   );
 };
