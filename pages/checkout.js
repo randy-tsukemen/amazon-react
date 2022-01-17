62477965f5ce258fbdaaf8f58964b570aecc73d4
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-18 01:31:41 +0900

    add image and container for checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,10 +1,26 @@
+import Image from "next/image";
 import Header from "../components/Header";
 
 const Checkout = () => {
   return (
     <div className="bg-gray-100">
       <Header />
-      <h1>checkout</h1>
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* Left */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+            alt=""
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-2xl border-b pb-4">Your Shopping Basket</h1>
+          </div>
+        </div>
+        {/* Right */}
+      </main>
     </div>
   );
 };
