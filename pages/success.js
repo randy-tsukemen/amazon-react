61015f553a5c42fc63430dcf43d66bab7242a124
Randy, Huang <gg875437@gmail.com>
2022-01-22 20:41:55 +0900

    add content to success page

--- pages/success.js
+++ pages/success.js
@@ -1,10 +1,31 @@
+import { CheckCircleIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 import React from "react";
 import Header from "../components/Header";
 
 const Success = () => {
+  const router = useRouter();
   return (
     <div className="bg-gray-100 h-screen">
       <Header />
+      <main className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col p-10 bg-white">
+          <div className="flex items-center space-x-2 mb-5">
+            <CheckCircleIcon className="text-green-500 h-10" />
+            <h1 className="text-3xl">
+              Thank you, your order has been confirmed!
+            </h1>
+          </div>
+          <p>
+            Thank you for shopping with us. We'll send a confirmation once your
+            item has shipped, if you would like to track your order please check
+            your email.
+          </p>
+          <button onClick={() => router.push("/orders")} className="button">
+            Go to my orders
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
