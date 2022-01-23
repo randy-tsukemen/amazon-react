7570b1a6b86ebd75447bcb11eed7be5fbfe9944c
Randy, Huang <gg875437@gmail.com>
2022-01-23 20:17:49 +0900

    load transaction data from firebase by SSR and fix firebase verison 9 method

---
+++ pages/orders.js
@@ -0,0 +1,97 @@
+import { getUnixTime } from "date-fns";
+import { getSession, useSession } from "next-auth/react";
+import React from "react";
+import Header from "../components/Header";
+import { db } from "../firebase";
+import {
+  collection,
+  query,
+  doc,
+  getDoc,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
+
+const Orders = ({ orders }) => {
+  const { data: session, status } = useSession();
+  console.log(orders);
+  return (
+    <div>
+      <Header />
+      <main className="max-w-screen-lg mx-auto p-10">
+        <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
+          Your Orders
+        </h1>
+
+        {session ? (
+          <h2>x Orders</h2>
+        ) : (
+          <h2>Please sign in to see your orders</h2>
+        )}
+        <div className="mt-5 space-y-4"></div>
+      </main>
+    </div>
+  );
+};
+
+export default Orders;
+
+export async function getServerSideProps(context) {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+  // Get the users logged in credentials
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
+
+  // Firebase db
+  // const stripeOrders = await db
+  //   .collection("users")
+  //   .doc(session.user.email)
+  //   .collection("orders")
+  //   .orderBy("timestamp", "desc")
+  //   .get();
+
+  const q = query(
+    collection(db, "users", session.user.email, "orders"),
+    orderBy("timestamp", "desc")
+  );
+
+  const stripeOrders = await getDocs(q);
+  // stripeOrders.forEach((doc) => {
+  //   // doc.data() is never undefined for query doc snapshots
+  //   console.log(doc.id, " => ", doc.data());
+  // });
+  // stripeOrders.docs.map((doc) => {
+  //   // doc.data() is never undefined for query doc snapshots
+  //   console.log(doc.id, " => ", doc.data());
+  // });
+
+  // Stripe orders
+  const orders = await Promise.all(
+    stripeOrders.docs.map(async (order) => ({
+      id: order.id,
+      amount: order.data().amount,
+      amountShipping: order.data().amount_shipping,
+      images: order.data().images,
+      timestamp: getUnixTime(order.data().timestamp.toDate()),
+      items: (
+        await stripe.checkout.sessions.listLineItems(order.id, {
+          limit: 100,
+        })
+      ).data,
+    }))
+  );
+
+  console.log(orders);
+
+  return {
+    props: {
+      orders,
+    },
+  };
+}
